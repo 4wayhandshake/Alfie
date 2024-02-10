14d12a0228900ea2b76193c1694d29a6ad08a0c1
4wayhandshake <timbo.in.space@proton.me>
2024-02-10 17:45:06 +0200

    Added colors and improved cookie/data parsing. Updated README

--- README.md
+++ README.md
@@ -1,41 +1,45 @@
-```
-           ,ggg,
-          dP""8I   ,dPYb, ,dPYb,                   ,,,,,,,,,,,
-         dP   88   IP'`Yb IP'`Yb                  : AUTOMATIC :
-        dP    88   I8  8I I8  8I   gg             : LOCAL     :
-       ,8'    88   I8  8' I8  8'   ""             : FILE      :
-       d88888888   I8 dP  I8 dP    gg    ,ggg,    : INCLUSION :
- __   ,8"     88   I8dP   I8dP     88   i8" "8i   : ENUMERATOR:
-dP"  ,8P      Y8   I8P    I8P      88   I8, ,8I    ````````````
-Yb,_,dP       `8b,,d8b,_ ,d8b,_  _,88,_ `YbadP'    by
- "Y8P"         `Y88P'"Y88PI8"88888P""Y8888P"Y888   4wayhandshake
-                          I8 `8,                        🤝🤝🤝🤝
-//#//#//#//#//#//#//#//#/ I8  `8, //#//#//#//#//
-                          I8   8I
-                          I8   8I
-                          I8, ,8'
-                           "Y8P'
-```
+![banner](images/banner.png)
 
-Scan websites for LFI vulnerabilities and path traversals. Multi-threaded for max speed; tasteful filters for max precision.
+Scan websites for LFI vulnerabilities and path traversals. 
+
+*Multi-threaded for max speed, tasteful filters for max precision.*
+
+---
 
 ## Usage
 
-Good fuzzing requires separating false-positives from the actual results. **Start with Alfie by determining which filters to apply** - just like you might do with `ffuf` or `gobuster`.
+Good fuzzing requires separating false-positives from the actual results. **Start by determining which filters to apply** - just like you might do with `ffuf` or `gobuster`. Do a **verbose** run to find some false-positives, for example:
 
-In this example, a known false-positive is identified, then it's size is used as a filter:
+```bash
+./alfie.py -u http://download.htb/files/download/ -f fuzz-list.txt --max 3 -t 60 --verbose
+```
 
-![false-positive](false-positive.gif)
+![false-positive](images/false-positive.gif)
 
-Any combination of HTTP status codes, word counts, and response sizes can be used as filters. 
+> Any combination of HTTP **status codes**, **word counts**, and response **sizes** can be used as filters. 
 
-Next, **apply the filter and do a non-verbose run**. In this example, the LFI stands out right away, leading to the hidden file `package.json`:
+Next, **apply some filters and do a non-verbose run**. In this example, the path traversal stands out right away, leading to the hidden files `package.json` and `app.js`:
 
-![lfi-test](lfi-test.gif)
+![lfi-test](images/lfi-test.gif)
+
+For logging of your results, use the `-o` or `--output` argument. The output is a text file with one positive result per line, and can be easily read by other scripts or programs. Here is a sample:
+
+```
+http://download.htb/files/download/%2e%2e%2fpackage.json
+http://download.htb/files/download/%2e%2e%2fapp.js
+http://download.htb/files/download/..%2fapp.js
+http://download.htb/files/download/..%2fpackage.json
+```
 
 
 
 ## Options
+
+By default, **Alfie** will output any non-default options you provide as arguments:
+
+![options display](images/options%20display.png)
+
+You can suppress this behavior and hide the banner with the `-q` or `--quiet` flag (in case you want to pipe the output to another program).
 
 ```html
   -h, --help            
@@ -53,34 +57,33 @@ Next, **apply the filter and do a non-verbose run**. In this example, the LFI st
 	This wordlist should have one filename per line, with file extensions if applicable.
 
   -w LFI_WORDLIST, --wordlist LFI_WORDLIST
-	Wordlist to use for LFI strings. 
-	If not using the default, it should be similar format to lfi-list.txt.
+	Wordlist to use for LFI strings. Default lfi-list.txt.
+	(If not using the default, it should be similar format to lfi-list.txt)
 
   -t THREADS, --threads THREADS
 	Number of threads to use for processing.
 
   --min MIN             
-	Minimum number of steps "back" to traverse.
+	Minimum number of steps "back" to traverse. Default 1.
 
   --max MAX             
-	Maximum number of steps "back" to traverse.
+	Maximum number of steps "back" to traverse. Default 10.
 
   --timeout TIMEOUT     
-	Timeout for each request (in seconds).
+	Timeout for each request (in seconds). Default 5s.
 
   --ending ENDING       
 	A character to append to the end of each test url. Ex. "%00".
 
-  -c COOKIES, --cookies COOKIES
-	Cookies to include in each request. 
-	Ex '{"key1": "value1", "key2": "value2"}'.
+  -b COOKIES, --cookies COOKIES
+	Cookies to include in each request. Ex. 'key1=value1; key2=value2'.
 
   -d DATA, --data DATA  
 	Data to include in each request. Only applies if using a POST request (see -X option).
-    Ex '{"key1": "value1", "key2": "value2"}'.
+    Ex. 'key1=value1; key2=value2'.
 
   -X REQUEST_TYPE, --request-type REQUEST_TYPE
-	Type of HTTP request to use. Ex "POST".
+	Type of HTTP request to use. Ex "POST". Defaults to "GET".
 
   -fs FILTER_SIZES, --filter-sizes FILTER_SIZES
 	Comma-separated list of sizes (in bytes) to filter from the results.
@@ -91,6 +94,15 @@ Next, **apply the filter and do a non-verbose run**. In this example, the LFI st
   -fc FILTER_CODES, --filter-codes FILTER_CODES
 	Comma-separated list of HTTP status codes to filter from the results.
 
+  -o OUTPUT, --output OUTPUT
+	File to log positive results.
+
+  --quiet
+	Don't print the banner or options.
+
+  -nc, --no-color
+	Don't ANSII colors in console output.
+
 ```
 
 
@@ -99,8 +111,7 @@ Next, **apply the filter and do a non-verbose run**. In this example, the LFI st
 
 - [x] Add a `-o` switch to output results to a file.
 - [x] Include a banner with all non-default options shown
-- [ ] Include a progress meter, with guesses per second and an estimated runtime.
-- [ ] Improve parsing of arguments `--cookie` and `--data` so that it isn't so strict about formatting.
+- [x] Improve parsing of arguments `--cookie` and `--data` so that it uses browser-like formatting.
 
 
 
--- alfie.py
+++ alfie.py
@@ -29,25 +29,35 @@ parser.add_argument('--min', dest='min', help='Minimum number of steps "back" to
 parser.add_argument('--max', dest='max', help='Maximum number of steps "back" to traverse.', type=int, default=10)
 parser.add_argument('--timeout', dest='timeout', help='Timeout for each request (in seconds).', type=int, default=5)
 parser.add_argument('--ending', dest='ending', help='A character to append to the end of each test url. Ex. "%%00".', type=str)
-parser.add_argument('-c', '--cookies', dest='cookies', help='Cookies to include in each request. Ex \'{"key1": "value1", "key2": "value2"}\'.', type=str)
-parser.add_argument('-d', '--data', dest='data', help='Data to include in each request. Only applies if using a POST request (see -X option). Ex \'{"key1": "value1", "key2": "value2"}\'.', type=str)
+parser.add_argument('-b', '--cookies', dest='cookies', help='Cookies to include in each request. Ex "key1=value1; key2=value2".', type=str)
+parser.add_argument('-d', '--data', dest='data', help='Data to include in each request. Only applies if using a POST request (see -X option). Ex "key1=value1; key2=value2".', type=str)
 parser.add_argument('-X', '--request-type', dest='request_type', help='Type of HTTP request to use. Ex "POST".', type=str, default='GET')
 parser.add_argument('-fs', '--filter-sizes', dest='filter_sizes', help='Comma-separated list of sizes (in bytes) to filter from the results.', type=str)
 parser.add_argument('-fw', '--filter-words', dest='filter_words', help='Comma-separated list of word counts to filter from the results.', type=str)
 parser.add_argument('-fc', '--filter-codes', dest='filter_codes', help='Comma-separated list of HTTP status codes to filter from the results.', type=str, default='400,401,402,403,404,405')
 parser.add_argument('-o', '--output', dest='output', help='File to log positive results.', type=str)
 parser.add_argument('--quiet', dest='quiet', action='store_true', help='Don\'t print the banner or options.')
+parser.add_argument('-nc', '--no-color', dest='colorless', action='store_true', help='Don\'t ANSII colors in console output.')
 
 args = parser.parse_args()
 
-banner = '''
+class colors:
+    RED = '\033[91m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    BLUE = '\033[94m'
+    MAGENTA = '\033[95m'
+    CYAN = '\033[96m'
+    END = '\033[0m'
+
+banner_colorless = '''
            ,ggg,
-          dP""8I   ,dPYb, ,dPYb,                   ,,,,,,,,,,,
-         dP   88   IP'`Yb IP'`Yb                  : AUTOMATIC :
-        dP    88   I8  8I I8  8I   gg             : LOCAL     :
-       ,8'    88   I8  8' I8  8'   ""             : FILE      :
-       d88888888   I8 dP  I8 dP    gg    ,ggg,    : INCLUSION :
- __   ,8"     88   I8dP   I8dP     88   i8" "8i   : ENUMERATOR:
+          dP""8I   ,dPYb, ,dPYb,                   ,,,,,,,,,,,,
+         dP   88   IP'`Yb IP'`Yb                  : AUTOMATIC  :
+        dP    88   I8  8I I8  8I   gg             : LOCAL      :
+       ,8'    88   I8  8' I8  8'   ""             : FILE       :
+       d88888888   I8 dP  I8 dP    gg    ,ggg,    : INCLUSION  :
+ __   ,8"     88   I8dP   I8dP     88   i8" "8i   : ENUMERATOR :
 dP"  ,8P      Y8   I8P    I8P      88   I8, ,8I    ````````````
 Yb,_,dP       `8b,,d8b,_ ,d8b,_  _,88,_ `YbadP'    by
  "Y8P"         `Y88P'"Y88PI8"88888P""Y8888P"Y888   4wayhandshake
@@ -58,6 +68,25 @@ Yb,_,dP       `8b,,d8b,_ ,d8b,_  _,88,_ `YbadP'    by
                           I8, ,8'
                            "Y8P'
 '''
+
+banner = '''\033[94m
+           ,ggg,
+          dP""8I   ,dPYb, ,dPYb,                  \033[92m ,,,,,,,,,,,,  \033[0m \033[94m
+         dP   88   IP'`Yb IP'`Yb                  \033[92m: AUTOMATIC  : \033[0m \033[94m
+        dP    88   I8  8I I8  8I   gg             \033[92m: LOCAL      : \033[0m \033[94m
+       ,8'    88   I8  8' I8  8'   ""             \033[92m: FILE       : \033[0m \033[94m
+       d88888888   I8 dP  I8 dP    gg    ,ggg,    \033[92m: INCLUSION  : \033[0m \033[94m
+ __   ,8"     88   I8dP   I8dP     88   i8" "8i   \033[92m: ENUMERATOR : \033[0m \033[94m
+dP"  ,8P      Y8   I8P    I8P      88   I8, ,8I   \033[92m ```````````` \033[0m \033[94m
+Yb,_,dP       `8b,,d8b,_ ,d8b,_  _,88,_ `YbadP'   \033[92m by \033[0m \033[94m
+ "Y8P"         `Y88P'"Y88PI8"88888P""Y8888P"Y888  \033[92m 4wayhandshake \033[0m \033[94m
+                          I8 `8,                        🤝🤝🤝🤝
+//#//#//#//#//#//#//#//#/ I8  `8, //#//#//#//#//
+                          I8   8I
+                          I8   8I
+                          I8, ,8'
+                           "Y8P'\033[0m
+'''
 # See https://manytools.org/hacker-tools/ascii-banner/ for banner creation
 # Font (nscript) by Normand Veilleux <nveilleu@emr.ca> Oct 7, 1994
 # figletized by Peter Samuelson <psamuels@hmc.edu> Nov 29, 1994
@@ -65,9 +94,36 @@ Yb,_,dP       `8b,,d8b,_ ,d8b,_  _,88,_ `YbadP'    by
 s = requests.session()
 exit_flag = False # Global flag to signal threads to exit
 successes = []
+
+# Use some globals to store info used in every request
 filter_codes = []
 filter_sizes = []
 filter_words = []
+request_cookie = None
+request_data = None
+
+def stringToDict(s):
+    # Parse a cookie-like string into a dict
+    key_value_pairs = [e.strip() for e in s.split(';')]
+    dict = {}
+    for kvp in key_value_pairs:
+        k, v = kvp.split('=',1)
+        # According to the HTTP cookie specification (RFC 6265),
+        # cookie values cant contain semicolon, comma, or whitespace
+        if any((c == ';' or c == ',' or c.isspace()) for c in v):
+            raise ValueError
+        dict[k] = v
+    if len(dict) == 0:
+        raise ValueError
+    return dict
+
+def parseCookie(s):
+    cookie_dict = stringToDict(s)
+    # return a requests cookiejar from a dictionary
+    return requests.utils.cookiejar_from_dict(cookie_dict)
+
+def parseData(s):
+    return stringToDict(s)
 
 def validateArgs():
 
@@ -109,17 +165,37 @@ def validateArgs():
             sys.exit(1)
 
     if args.cookies:
+        global request_cookie
         try:
-            json.loads(args.cookies)
-        except:
-            print('Invalid cookie provided. Please use a format like \'{"key1": "value1", "key2": "value2"}\'')
+            request_cookie = parseCookie(args.cookies)
+        except ValueError as e:
+            if args.colorless:
+                print(f'Warning: invalid cookies provided.\nPlease use this format: "key1=value1; key2=value2".\nProceeding without cookies.\n')
+            else:
+                print(f'{colors.MAGENTA}Warning: invalid cookies provided.{colors.END}\nPlease use this format: "key1=value1; key2=value2".\nProceeding without cookies.\n')
+            request_cookie = None
+        except Exception as e:
+            if args.colorless:
+                print(f'An error occurred while parsing cookies:\n{e}\n')
+            else:
+                print(f'{colors.MAGENTA}An error occurred while parsing cookies:{colors.END}\n{e}\n')
             sys.exit(1)
 
     if args.data:
+        global request_data
         try:
-            json.loads(args.data)
-        except:
-            print('Invalid data provided. Please use a format like \'{"key1": "value1", "key2": "value2"}\'')
+            request_data = parseData(args.data)
+        except ValueError as e:
+            if args.colorless:
+                print(f'Warning: invalid request data provided.\nPlease use this format: "key1=value1; key2=value2".\nProceeding without request data.\n')
+            else:
+                print(f'{colors.MAGENTA}Warning: invalid request data provided.{colors.END}\nPlease use this format: "key1=value1; key2=value2".\nProceeding without request data.\n')
+            request_data = None
+        except Exception as e:
+            if args.colorless:
+                print(f'An error occurred while parsing request data:\n{e}\n')
+            else:
+                print(f'{colors.MAGENTA}An error occurred while parsing request data:{colors.END}\n{e}\n')
             sys.exit(1)
 
 def loadWordlist(filename):
@@ -146,26 +222,36 @@ def matches(resp, url):
         ret = False
     if ret:
         if args.verbose:
-            print(f"\n[+] {url}\n    HTTP {code:<8} Size: {bytes:<12} Words: {words:<12}")
+            if args.colorless:
+                print(f"{' '*64}\n[+] {url:<60}\n    HTTP {code:<8} Size: {bytes:<12} Words: {words:<20}")
+            else:
+                print(f"{' '*64}\n{colors.GREEN}[+] {url:<60}{colors.END}\n    HTTP {code:<8} Size: {bytes:<12} Words: {words:<20}")
         else:
-            print(f"[+] {url}")
+            if args.colorless:
+                print(f"[+] {url:<60}")
+            else:
+                print(f"{colors.GREEN}[+] {url:<60}{colors.END}")
     elif args.verbose:
-        print(f"\n[-] {url}\n    HTTP {code:<8} Size: {bytes:<12} Words: {words:<12}")
+        if args.colorless:
+            print(f"{' '*64}\n[-] {url:<60}\n    HTTP {code:<8} Size: {bytes:<12} Words: {words:<20}")
+        else:
+            print(f"{' '*64}\n{colors.RED}[-] {url:<60}{colors.END}\n    HTTP {code:<8} Size: {bytes:<12} Words: {words:<20}")
     return ret
 
 def makeRequest(url):
-    c = json.loads(args.cookies) if args.cookies else None
-    d = json.loads(args.data) if args.data else None
     try:
         if args.request_type.upper() == 'POST':
-            response = requests.post(url, cookies=c, json=d, timeout=args.timeout)
+            response = requests.post(url, cookies=request_cookie, json=request_data, timeout=args.timeout)
         else:
-            response = requests.get(url, cookies=c, timeout=args.timeout)
+            response = requests.get(url, cookies=request_cookie, timeout=args.timeout)
         if matches(response, url):
             global successes
             successes.append(url)
     except requests.RequestException as e:
-        print(f"An error occurred: {e}")
+        if args.colorless:
+            print(f'An error occurred while making a request to {url}:\n{e}\n')
+        else:
+            print(f'{colors.MAGENTA}An error occurred while making a request to {url}:{colors.END}\n{e}\n')
 
 def processUrls(url_queue):
     global exit_flag
@@ -241,7 +327,10 @@ def printOptions():
 
 def main():
     if not args.quiet:
-        print(banner)
+        if args.colorless:
+            print(banner_colorless)
+        else:
+            print(banner)
         printOptions()
     # Register the signal handler for SIGINT
     signal.signal(signal.SIGINT, signalHandler)
@@ -261,7 +350,8 @@ def main():
                 url = f'{args.url}{depth*w}{f}{ending}'
                 url_queue.put(url)
     # Using ThreadPoolExecutor for asynchronous web requests with a limited number of threads
-    N = min(args.threads,url_queue.qsize())
+    num_jobs = url_queue.qsize()
+    N = min(args.threads, num_jobs)
     with concurrent.futures.ThreadPoolExecutor(max_workers=N) as executor:
         # Submit the process_urls function for each worker thread
         workers = [executor.submit(processUrls, url_queue) for _ in range(N)]
@@ -279,9 +369,11 @@ def main():
         concurrent.futures.wait(workers)
     if args.output:
         writeLogfile(args.output, successes)
+    return num_jobs
 
 if __name__ == "__main__":
     start_time = time.time()
-    main()
+    n = main()
     end_time = time.time()
-    print(f"\nElapsed: {end_time - start_time:.1f}s")
+    elapsed = end_time - start_time
+    print(f"\nComplete: {n} requests in {elapsed:.1f}s ({n/elapsed:.0f} req/s)")
